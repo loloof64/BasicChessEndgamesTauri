94eeeabdcfa03ffcbab68a8a8b4cbb2ec079a949
Laurent Bernabe <laurent.bernabe@gmail.com>
2022-06-29 15:59:45 +0200

    Showing a snack bar on new game

--- src/components/pages/GamePage.vue
+++ src/components/pages/GamePage.vue
@@ -8,10 +8,14 @@ defineProps({
 
 const gameStore = useStore();
 const board = ref();
+const snackBarOpen = ref(false);
+const snackBarMessage = ref('');
 
 function startNewGame() {
   gameStore.startNewGame('rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2');
   board.value.newGame(gameStore.startPosition);
+  snackBarMessage.value = "New game started."
+  snackBarOpen.value = true;
 }
 </script>
 
@@ -25,6 +29,12 @@ function startNewGame() {
     <ui-button raised @click="startNewGame()">New game</ui-button>
   </p>
   <p>Start position is <b>{{ gameStore.startPosition }}</b></p>
+  <ui-snackbar
+    v-model="snackBarOpen"
+    :timeout-ms="4000"
+    :message="snackBarMessage"
+    :action-type="1"
+  ></ui-snackbar>
 </template>
 
 <style scoped>
